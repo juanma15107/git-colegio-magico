e67fa8a108fb36c18ded4d17e67f03689036d485
juanma <juanma15107@gmail.com>
2026-09-09 15:32:03 -0500

    feat: trato de hacer esto pero no me deja subirlo :´(

--- src/main/java/com/colegiomagico/reto7/CofreVaritas.java
+++ src/main/java/com/colegiomagico/reto7/CofreVaritas.java
@@ -1,12 +1,18 @@
 package com.colegiomagico.reto7;
 
 /**
- * Retorna el número mayor dentro del arreglo de tamaños.
+ 
  */
 public class CofreVaritas {
 
     public static int varitaMasPoderosa(int[] tamanos) {
-        return 0; // TODO: Cambia esto
+        int max = tamanos[0];
+        for (int i = 1; i < tamanos.length; i++) {
+            if (tamanos[i] > max) {
+                max = tamanos[i];
+            }
+        }
+        return max;
     }
 
     public static void main(String[] args) {
